69a861285efb1612e84a8a04833732dd973af6a0
mranuj123 <dotsyko@gmail.com>
2024-11-15 15:57:34 +0545

    delete file5txt and remove from cached demo_not_include js

--- demo/demo_not_include.js
+++
@@ -1,7 +0,0 @@
-<script>
-    console.log("not include");
-    console.log ("hello world");
-    console.log ("hello world added once again");
-    console.log ("hello world added once again");
-
-</script>
